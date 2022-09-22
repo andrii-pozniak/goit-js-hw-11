b5366b2d9651eeffe196b1344a24c6a8b42b6957
andrii-pozniak <andriipozniak@gmail.com>
2022-09-22 23:33:09 +0300

    add refs

--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -1,3 +1,8 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
+import getRefs from "./getRefs";
+
+const refs = getRefs()
+console.log(refs.form);
+console.log(refs.inputRequest);
---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+    return {
+        inputRequest: document.querySelector(`input`),
+        form: document.querySelector(`#search-form`)
+    }
+    
+}
